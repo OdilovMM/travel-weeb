8973136ceabad6e0690d6f7d6bc0599fd730c888
OdilovMM <dssrinman@mail.com>
2023-06-16 01:21:33 +0500

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>Journey Bound</h1>
+        <p>Embark on Limitless Adventures and Unforgettable Experiences.</p>
+        <a href="#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
